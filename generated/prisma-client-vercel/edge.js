68467441cc4a961c3392574ccab2145c6e6b1e98
Matěj Nevlud <matej.nevlud@bico.digital>
2024-09-25 16:21:13 +0200

    venv vercel ?

--- generated/prisma-client-vercel/edge.js
+++ generated/prisma-client-vercel/edge.js
@@ -385,7 +385,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../.env",
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
--- generated/prisma-client-vercel/index.js
+++ generated/prisma-client-vercel/index.js
@@ -386,7 +386,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../.env",
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
